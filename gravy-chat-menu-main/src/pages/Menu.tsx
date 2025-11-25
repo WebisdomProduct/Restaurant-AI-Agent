8ea67327a804f990809f2b807211528803963767
vig328 <vignesh.loharkar123@gmail.com>
2025-11-25 17:30:09 +0530

    Update API endpoint for menu data fetching

--- gravy-chat-menu-main/src/pages/Menu.tsx
+++ gravy-chat-menu-main/src/pages/Menu.tsx
@@ -25,7 +25,7 @@ const Menu = ({ onAddToCart }: MenuProps) => {
   useEffect(() => {
     const loadMenu = async () => {
       try {
-        const res = await fetch("http://34.180.6.75:8000/api/menu");
+        const res = await fetch("http://34.14.219.235:8000/api/menu");
         const data = await res.json();
 
         if (!Array.isArray(data)) {
